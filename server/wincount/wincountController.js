e730f2ac9d2038a46b6d3659b9d9db864de6b042
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-08-18 08:35:35 -0700

    Return JSON object of nodes and links from Wincount API route.

--- server/wincount/wincountController.js
+++ server/wincount/wincountController.js
@@ -5,6 +5,7 @@
 // the MongoDB database.
 
 var Wincount = require('./wincountModel.js');
+var Player = require('../player/playerModel.js');
 
 module.exports = {
 
@@ -12,11 +13,64 @@ module.exports = {
   getAllWincounts: function(req, res) {
     Wincount.findAsync()
       .then(function(wincounts) {
-        res.json(wincounts);
+        // Given array of wincounts for all players
+        // convert into nodes and links JSON object needed by D3.js graph
+        convertToGraphData(res, wincounts);
       })
       .catch(function(err){
         console.error('Error in reading wincounts');
       });
   }
-
 };
+
+// Given a JSON object of wincounts for all players
+// convert into nodes and links JSON object
+function convertToGraphData(res, wincounts) {
+  
+  // Arrays to store nodes and links needed for D3.js graph
+  var nodes = [];
+  var links = [];
+
+  var sourceIndex, targetIndex;
+
+  // Get distinct set of players
+  Player.findAsync()
+  .then(function(players) {
+    players.forEach(function(player) {
+      nodes.push({ 'name': player.name});
+    });
+  })
+  .then(function() {
+    // Based on wincounts array create links array with source and target indices and wincount value
+    wincounts.forEach(function(record) {
+      console.log(record);
+      sourceIndex = getIndexByProperty(nodes, 'name', record.player);
+      targetIndex = getIndexByProperty(nodes, 'name', record.opponent);
+      links.push({
+        'source': sourceIndex,
+        'target': targetIndex,
+        'value': record.wincount
+      });
+    });
+
+    // Create JSON object of nodes and links to return
+    var result = {
+      'nodes': nodes,
+      'links': links
+    };
+
+    res.json(result);
+  })
+  .catch(function(err) {
+    console.error('Error in finding all players:', err);
+  });
+}
+
+function getIndexByProperty(arr, prop, value) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i][prop] === value) {
+      return i;
+    }
+  }
+  return -1;
+}
