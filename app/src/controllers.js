f50fa6812850fdb7c05ce514caacf062bde6991c
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-08-18 12:33:12 -0700

    Add ability to record new matches.

--- app/src/controllers.js
+++ app/src/controllers.js
@@ -18,12 +18,34 @@ angular.module('foositControllers', [])
     
     $scope.matchList = [];
 
-    MatchService.getMatches()
-      .then(function(matches) {
-        $scope.matchList = matches.data;
-      })
-      .catch(function(err) {
-        console.error('Error in getting matches:', err);
-      });
+    function refreshMatchList() {
+      MatchService.getMatches()
+        .then(function(matches) {
+          $scope.matchList = matches.data;
+        })
+        .catch(function(err) {
+          console.error('Error in getting matches:', err);
+        });
+
+    $scope.addNewMatch = function() {
+      var newMatch = {
+        date: $scope.newDate,
+        winner: $scope.newWinner,
+        winnerScore: $scope.newScoreW,
+        loser: $scope.newLoser,
+        loserScore: $scope.newScoreL
+      };
+
+      MatchService.addMatch(newMatch)
+        .then(function() {
+          console.log('Match added successfully');
+          refreshMatchList();
+        })
+    };
+
+    }
+
+    // Populate $scope with latest match list on controller load
+    refreshMatchList();
 
   }]);
--- app/src/services.js
+++ app/src/services.js
@@ -10,4 +10,8 @@ angular.module('foositServices', [])
     this.getMatches = function() {
       return $http.get('/api/match');
     };
+
+    this.addMatch = function(match) {
+      return $http.post('/api/match/add', match);
+    }
   }]);
